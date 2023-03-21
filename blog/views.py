d11351f761ea281c3012a474ab6e0966b6f4c86d
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-03-20 22:40:42 -0700

    [ATTEMPT] To display posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from .models import Post
 from . import db
@@ -9,8 +9,11 @@ views = Blueprint("views", __name__)
 @views.route("/home")
 @login_required
 def home():
+    # Get all posts
+    posts = Post.query.all()
+
     # Changed base.html to index.html for tailwind purposes
-    return render_template("home.html", user=current_user)
+    return render_template("home.html", user=current_user, posts=posts)
 
 @views.route("/create-post")
 @login_required
@@ -26,6 +29,7 @@ def create_post():
             db.session.add(post)
             db.session.commit()
             flash('Post created!', category='success')
+            return redirect(url_for('view.home'))
 
 
     return render_template('create_post.html', user=current_user)
