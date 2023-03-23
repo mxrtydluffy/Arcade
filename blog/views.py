aa9c2571bef1d22930633f2c18ba214a54e5718e
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-03-23 00:49:30 -0700

    [UPDATE] Some features & styling

--- blog/views.py
+++ blog/views.py
@@ -79,3 +79,19 @@ def create_comment(post_id):
             flash('Post does not exist.', category='error')
 
     redirect(url_for('views.home'))
+
+@ views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Comment does not exist.', category='error')
+    # Unauthorized user
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('You do not have permission to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+    
+    return redirect(url_for('views.home'))
